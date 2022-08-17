7b48b9657b29af59eaba698d24411e13c8458d5a
tengtengsnake <63833307+tengtengsnake@users.noreply.github.com>
2022-08-17 21:00:40 +0800

    python的dict剛好可以完整呈現Hashtable

---
+++ ch8_1.py
@@ -0,0 +1,12 @@
+product_list={}  #產品列表字典  建立一個空字串
+product_list['Refregerator']=8000
+product_list['Television']=12000
+product_list['Printer']=8000
+print("列印產品資料")
+print(product_list)
+print("列印Refregerator:",product_list['Refregerator'])
+print("列印Television:",product_list['Television'])
+print("列印Printer:",product_list['Printer'])
+#dict
+#dict={'test':1000,'key':10002,'test2':12000}  #key value pair ->dictionary
+#print(dict)
