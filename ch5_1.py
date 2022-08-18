991e28f39746389765e525dc720c7f836686f3c3
tengtengsnake <63833307+tengtengsnake@users.noreply.github.com>
2022-08-18 14:57:07 +0800

    Stack是FILO ru,6e.4
    append()將資料加入至堆疊末端
    pop() 打出末端資料並從stack中移除

---
+++ ch5_1.py
@@ -0,0 +1,9 @@
+fruits=[]  #建立空串列實踐stack
+fruits.append('Grape')
+fruits.append('Mango')
+fruits.append('Apple')#append()將資料加入至堆疊末端
+print("列印fruits=",fruits)  #print out the list(fruits)
+print("pop operating",fruits.pop())
+print("pop operating",fruits.pop())
+print("pop operating",fruits.pop())
+#pop() 打出末端資料並從stack中移除
