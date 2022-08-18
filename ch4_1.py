8b77d4b976a99ec9aee106c961a1952ee64028b9
tengtengsnake <63833307+tengtengsnake@users.noreply.github.com>
2022-08-18 11:11:23 +0800

    Update and rename ch4_3.py to ch4_1.py

--- ch4_1.py
+++ ch4_1.py
@@ -13,3 +13,5 @@ q.enqueue('Grape')
 q.enqueue('Mango')
 q.enqueue('Apple')
 print("the len of Queue is:",q.size())
+#利用Queue class 建立物件q,q可以使用class 的東西,有了初始化函數後,物件q就有了個空串列
+#函數有self,是為了搞清楚是哪一個物件在使用類別中的函數
