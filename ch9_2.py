a9f8219f1fc1368fac96a1c4a91edabe6a2c8adc
tengtengsnake <63833307+tengtengsnake@users.noreply.github.com>
2022-08-21 01:09:00 +0800

    使用built_in sort()
    使用內建sort(),預設是由小排到大

---
+++ ch9_2.py
@@ -0,0 +1,10 @@
+cars=['honda','bmw','toyota','ford']
+print("目前串列內容=",cars)
+print("使用內建sort() 進行排序")
+cars.sort()
+print("排序結果",cars)
+
+nums=[5,3,9,2]
+print("使用sort()由小排到大")
+nums.sort()
+print("排序結果=",nums)     
