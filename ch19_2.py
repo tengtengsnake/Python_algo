884a2383a2f2e3e5ce9780808d1b0fcd2999ad28
tengtengsnake <63833307+tengtengsnake@users.noreply.github.com>
2022-08-21 10:20:16 +0800

    The example of the palindrome(回文)

---
+++ ch19_2.py
@@ -0,0 +1,14 @@
+#回文Palindrome
+from collections import deque
+
+def palindrome(word):
+    wd=deque(word) #打出queue
+    while len(wd)>1:
+        if wd.pop()==wd.popleft():#pop 從末端讀取queue資料,popleft() 則是從左端讀取queue資料
+            return True
+        else:
+            return False
+print("x is palindrome:",palindrome("x"))
+print("abccba is palindrome",palindrome("abccba"))
+print("radar is palindrome",palindrome("radar"))
+print("python is palindrome",palindrome("python"))
